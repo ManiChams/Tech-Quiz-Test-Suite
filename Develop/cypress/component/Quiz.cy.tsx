bcdf8a60e92c318398d65abf7d3d63b50d8e8ba6
ManiChams <kimanichambliss@gmail.com>
2024-12-09 15:47:57 -0500

    initial commit

---
+++ Develop/cypress/component/Quiz.cy.tsx
@@ -0,0 +1,23 @@
+
+import Quiz from '../../client/src/components/Quiz';
+import { mount } from 'cypress/react';
+
+describe('Quiz Component', () => {
+  it('renders Start Quiz button', () => {
+    mount(<Quiz />);
+    cy.contains('Start Quiz').should('be.visible');
+  });
+});
+
+it('loads the questions and initializes the quiz', () => {
+  mount(<Quiz />);
+  cy.contains('Start Quiz').click();
+  cy.intercept('GET', '/api/questions/random', { fixture: 'quiz.json' });
+  cy.get('button').should('start');
+});
+
+
+it('displays questions correctly', () => {
+  mount(<Quiz />);
+  cy.get('question').should('have.length, 4');
+});
